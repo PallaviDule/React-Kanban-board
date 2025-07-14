f7fd381bc20b7e7473a45f67af9b25413889c304
Pallavi <pallavidule@gmail.com>
2025-07-14 19:17:54 +0200

    css header

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,17 @@ import Board from './components/Board'
 function App() {
 
   return (
-    <>
-       <h1 className="text-2xl font-bold mb-4">Kanban Board</h1>
-      <Board />
-    </>
+<div className="h-screen flex flex-col">
+      {/* Fixed App Header */}
+      <header className="sticky top-0 z-10 bg-white shadow px-6 py-2">
+        <h1 className="text-2xl font-bold text-blue-800">Kanban Board</h1>
+      </header>
+
+      {/* Scrollable main content */}
+      <main className="flex-1 overflow-auto">
+        <Board />
+      </main>
+    </div>
   )
 }
 
